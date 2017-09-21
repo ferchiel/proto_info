cf7396ef41bf8144d30dce963cd9c777f50f8bc2
root <root@DESKTOP-LSPIOS8.localdomain>
2017-09-21 22:46:39 +0800

    first

---
+++ test/proto_info.lua
@@ -0,0 +1,23 @@
+-- automake by excel2lua
+-- auth: ferchiel	mail: ferchiel@163.com
+
+return {
+	info = {
+		[1038450125] = {
+			action = 'test',
+			proto = 'test',
+			module = 'testd',
+			request = 'request_test',
+			response = 'response_test',
+		},
+		[-552924107] = {
+			action = 'boring',
+			proto = 'test',
+			module = 'testd',
+			push = 'push_boring',
+		},
+	},
+	files = {
+		'test.proto',
+	},
+}
